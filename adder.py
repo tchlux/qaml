acac745a838167fb8600dee282bd4d139a6b03c0
sindhura97 <42525474+sindhura97@users.noreply.github.com>
2019-04-02 21:19:33 -0400

    Added n-bit integer addition of two nums with 4n qubits

--- adder.py
+++ adder.py
@@ -1,5 +1,6 @@
 from qubo import QUBO
 from qubo import run_qubo
+from exceptions import UsageError
 
 
 # Given four bit indices (integers), generate a QUBO for x+y=s+2c.
@@ -19,10 +20,30 @@ def full_adder(x=1, y=2, z=3, s=4, c=5):
                  f"b{z}b{s}" : -2, f"b{z}b{c}" : -4,
                  f"b{s}b{c}" : 4,
                  })
+
+# Given bit indices x1,x2,..,xn,y1,y2,...yn,s1,s2,...sn,c1,c2,...cn
+# Generate a QUBO for x, y, s, c.
+# Each of x, y, s, c is n bits wide.
+# Uses 4n qubits.
+def two_int_adder_4n(*bit_indices):
+    if len(bit_indices)%4!=0:
+        raise UsageError('No. of input indices should be a multiple of 4.')
+    n = len(bit_indices) // 4
+    full_qubo = half_adder(bit_indices[n-1], bit_indices[2*n-1], 
+                           bit_indices[3*n-1], bit_indices[4*n-1])
+    for i in range(n-1):
+        full_qubo += full_adder(bit_indices[i], bit_indices[i+n], 
+                                bit_indices[i+3*n+1], #carry bit
+                                bit_indices[i+2*n], bit_indices[i+3*n])
+    return full_qubo
+    
     
 if __name__=='__main__':
     print ('=====half-adder')
     run_qubo(**half_adder())
     print ('=====full-adder')
     run_qubo(**full_adder())
+    print ('=====two n-bit integer addition with 4n qubits')
+    bit_indices = list(range(1,4*3+1))
+    run_qubo(**two_int_adder_4n(*bit_indices))
 
