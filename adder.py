22ee21c8b6d8162c47fd332902c28d5740987217
sindhura97 <42525474+sindhura97@users.noreply.github.com>
2019-04-04 10:51:53 -0400

    Update adder.py

--- adder.py
+++ adder.py
@@ -13,33 +13,49 @@ def two_int_adder_4n(*bit_indices):
     if len(bit_indices)%4!=0:
         raise UsageError('No. of input indices should be a multiple of 4.')
     n = len(bit_indices) // 4
-#    full_qubo = half_adder(bit_indices[n-1], bit_indices[2*n-1], 
-#                           bit_indices[3*n-1], bit_indices[4*n-1])
-#    for i in range(n-1): 
-#        full_qubo += full_adder(bit_indices[i], bit_indices[i+n], 
-#                                bit_indices[i+3*n+1], #carry bit
-#                                bit_indices[i+2*n], bit_indices[i+3*n])
-#    return full_qubo
-    qubo_rep = dict()
-    for i in range(n):
-        qubo_rep[f"a{bit_indices[i]}"] = 1
-        qubo_rep[f"a{bit_indices[i+n]}"] = 1
-        qubo_rep[f"a{bit_indices[i+2*n]}"] = 1
-        qubo_rep[f"a{bit_indices[i+3*n]}"] = 5 #adjusted later for c1
-        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+n]}"] = 2
-        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+2*n]}"] = -2
-        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+3*n]}"] = -4
-        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+2*n]}"] = -2
-        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+3*n]}"] = -4
-        qubo_rep[f"b{bit_indices[i+2*n]}b{bit_indices[i+3*n]}"] = 4
-    qubo_rep[f"a{bit_indices[3*n]}"] = 4
-    for i in range(n-1):
-        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+1+3*n]}"] = 2
-        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+1+3*n]}"] = 2
-        qubo_rep[f"b{bit_indices[i+2*n]}b{bit_indices[i+1+3*n]}"] = -2
-        qubo_rep[f"b{bit_indices[i+3*n]}b{bit_indices[i+1+3*n]}"] = -4
-    return QUBO(qubo_rep)
+    full_qubo = half_adder(bit_indices[n-1], bit_indices[2*n-1], 
+                           bit_indices[3*n-1], bit_indices[4*n-1])
+    for i in range(n-1): 
+        full_qubo += full_adder(bit_indices[i], bit_indices[i+n], 
+                                bit_indices[i+3*n+1], #carry bit
+                                bit_indices[i+2*n], bit_indices[i+3*n])
+    return full_qubo
+ 
     
+# Given bit indices x1,x2,..,xn,y1,y2,...ym,s1,s2,...sn,c1,c2,...cn
+# where n>m
+# Generate a QUBO for x, y, s, c such that x+y = c1,s
+# Uses 3n+m qubits 
+def skewed_two_int_adder(n,m,*bit_indices):
+    if n<m: 
+        raise UsageError('It is required that n>=m')
+    elif len(bit_indices)!=3*n+m:
+        raise UsageError('No. of input indices should be 3n+m.')
+    final_qubo = half_adder(bit_indices[n-1], bit_indices[n-1+m],
+                            bit_indices[2*n-1+m], bit_indices[3*n-1+m])
+    for i in range(m-1):
+        final_qubo += full_adder(bit_indices[n-m+i], bit_indices[n+i],
+                                 bit_indices[3*n+i+1],
+                                 bit_indices[2*n+i], bit_indices[3*n+i])
+    for i in range(n-m):
+        final_qubo += half_adder(bit_indices[i], bit_indices[2*n+m+i+1],
+                                 bit_indices[n+m+i], bit_indices[2*n+m+i])
+    return final_qubo
+    
+    
+    
+    
+
+# Given bit indices 
+# x11, x12, ..., x1n
+# x21, x22, ..., x2n
+# .
+# .
+# xm1, xm2, ..., xmn
+def m_int_adder(*bit_indices):
+    pass
+
+
     
 if __name__=='__main__':
     print ('=====half-adder')
@@ -49,4 +65,33 @@ if __name__=='__main__':
     print ('=====two n-bit integer addition with 4n qubits')
     bit_indices = list(range(1,4*2+1))
     run_qubo(**two_int_adder_4n(*bit_indices))
+    print ('=====two n,m bit integer addition with 3*n+m qubits')
+    n,m = 3,2
+    bit_indices = list(range(1,3*n+m+1))
+    run_qubo(**skewed_two_int_adder(n,m,*bit_indices))
+    
 
+
+
+## alternate implementation of two_int_adder_4n unrolling the 
+# addition of adders
+#def two_int_adder_4n(*bit_indices):
+#    qubo_rep = dict()
+#    for i in range(n):
+#        qubo_rep[f"a{bit_indices[i]}"] = 1
+#        qubo_rep[f"a{bit_indices[i+n]}"] = 1
+#        qubo_rep[f"a{bit_indices[i+2*n]}"] = 1
+#        qubo_rep[f"a{bit_indices[i+3*n]}"] = 5 #adjusted later for c1
+#        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+n]}"] = 2
+#        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+2*n]}"] = -2
+#        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+3*n]}"] = -4
+#        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+2*n]}"] = -2
+#        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+3*n]}"] = -4
+#        qubo_rep[f"b{bit_indices[i+2*n]}b{bit_indices[i+3*n]}"] = 4
+#    qubo_rep[f"a{bit_indices[3*n]}"] = 4
+#    for i in range(n-1):
+#        qubo_rep[f"b{bit_indices[i]}b{bit_indices[i+1+3*n]}"] = 2
+#        qubo_rep[f"b{bit_indices[i+n]}b{bit_indices[i+1+3*n]}"] = 2
+#        qubo_rep[f"b{bit_indices[i+2*n]}b{bit_indices[i+1+3*n]}"] = -2
+#        qubo_rep[f"b{bit_indices[i+3*n]}b{bit_indices[i+1+3*n]}"] = -4
+#    return QUBO(qubo_rep)
