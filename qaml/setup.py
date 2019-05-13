cae75716ecca5cfa40be5a4f64584079f082ff2f
Thomas Lux <thomas.ch.lux@gmail.com>
2019-05-13 09:27:54 -0400

    Updated 'setup.py' to use a local user token file. Removed Old files.

--- qaml/setup.py
+++ qaml/setup.py
@@ -1,7 +1,15 @@
 # Personal account token string (https://cloud.dwavesys.com/leap/) for
 # running experiments on real DWAVE system.
-token = ""
+import os
 
-if (len(token) == 0):
+token_file_name = ".dwave-token"
+token_file = os.path.join(os.expanduser("~"), token_file_name)
+
+if not os.path.exists(token_file):
     class UnregisteredAccount(Exception): pass
-    raise(UnregisteredAccount("Store your API token string in the 'token' variable after registering an account with DWAVE Leap."))
+    raise(UnregisteredAccount("\n\nStore your DWAVE Leap API token string in a file named '.dwave-token' in your home directory.\nGet this token by registering an account with DWAVE Leap."))
+
+# Read the token from a file on the user's computer.
+with open(token_file) as f:
+    token = f.read().strip()
+
